27a5bf10b78760c4c93c4b40b78d7c679463b335
Chiaki Mizuta <chiaki.mizuta@holition.com>
2019-07-28 17:52:04 +0100

    edited stack class to be a queue class

--- stack_template.hpp
+++ stack_template.hpp
@@ -2,47 +2,47 @@
 
 //Constructor
 template <class Type, int maxsize>
-stack<Type, maxsize>::stack()
-	:stack_pointer(-1)
+queue<Type, maxsize>::queue()
+	:queue_pointer(0), end_queue(0)
 {}
 
 //Destructor
 template <class Type, int maxsize>
-stack<Type, maxsize>::~stack()
+queue<Type, maxsize>::~queue()
 {}
 
 template <class Type, int maxsize>
-BOOL stack<Type, maxsize>::empty() const
+BOOL queue<Type, maxsize>::empty() const
 {
-	return stack_pointer == -1 ? TRUE : FALSE;
+	return queue_pointer == end_queue ? TRUE : FALSE;
 }
 
 template <class Type, int maxsize>
-BOOL stack<Type, maxsize>::full() const
+BOOL queue<Type, maxsize>::full() const
 {
-	return stack_pointer == maxsize -1 ? TRUE : FALSE;
+	return end_queue == maxsize - 1 ? TRUE : FALSE;
 }
 
 template <class Type, int maxsize>
-BOOL stack<Type, maxsize>::pop(Type& top_elem)
+BOOL queue<Type, maxsize>::pop(Type& bottom_elem)
 {
 	if(empty())
 	{
 		std::cout << "Stack empty: Pop failed.\n";
 		return FALSE;
 	}
-	top_elem = element[stack_pointer--];
+	bottom_elem = element[queue_pointer++];
 	return TRUE;
 }
 
 template <class Type, int maxsize>
-BOOL stack<Type, maxsize>::push(const Type& new_elem)
+BOOL queue<Type, maxsize>::push(const Type& new_elem)
 {
 	if(full())
 	{
 		std::cout << "Stack full: Push failed.\n";
 		return FALSE;
 	}
-	element[++stack_pointer] = new_elem;
+	element[end_queue++] = new_elem;
 	return TRUE;
 }
